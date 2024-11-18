652a9bc68d1c2b51249735d3705b20817f1252ee
Dario Amini <darioamini@yahoo.co.uk>
2024-11-18 15:24:50 +0000

    Centred images

--- src/components/square/Droppable.tsx
+++ src/components/square/Droppable.tsx
@@ -4,6 +4,7 @@ import { useContext } from 'react';
 import { SquareContext } from '../../contexts/squareContext';
 import { SquareData } from '../../types';
 import DraggableImage from './DraggableImage';
+import "./Droppable.css";
 
 type Props = {
     dropId: Exclude<keyof SquareData, "stagingArea">
@@ -24,7 +25,7 @@ function Droppable({ dropId }: Props) {
     const itemInfo = squareData[dropId]
 
     return (
-        <div ref={setNodeRef} style={style}>
+        <div className='sub-square' ref={setNodeRef} style={style}>
             {itemInfo ? <DraggableImage imageUrl={itemInfo.imageUrl} /> : null}
         </div>
     );
