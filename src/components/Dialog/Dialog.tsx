c3abc1e61ba735c4d42ddadae7f4906c497b4951
Dario Amini <darioamini@yahoo.co.uk>
2024-11-19 23:58:01 +0000

    Changed load from clipboard to use dialog

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,23 @@
+import { forwardRef, PropsWithChildren } from "react"
+
+
+export const Dialog = forwardRef<HTMLDialogElement, PropsWithChildren>(function Dialog({ children }, ref) {
+    return <dialog ref={ref}>{children}</dialog>
+})
+
+
+function useDialog(ref: HTMLDialogElement | null): [open: () => void, close: () => void] {
+
+    function open() {
+        ref?.showModal()
+    }
+
+    function close() {
+        ref?.close()
+    }
+
+    return [open, close]
+}
+
+
+export default useDialog
--- src/components/Navbar/LoadFromClipboardButton.tsx
+++ src/components/Navbar/LoadFromClipboardButton.tsx
@@ -1,27 +1,54 @@
-import { useContext, useState } from "react"
+import { useContext, useRef, useState } from "react"
 import { SquareContext } from "../../contexts/squareContext"
+import useDialog, { Dialog } from "../Dialog/Dialog"
 import { isSquareData } from "../square/squareUtils"
+import "./LoadFromClipboardButton.css"
 
 function LoadFromClipboardButton() {
     const { setSquareData } = useContext(SquareContext)
-    const [label, setLabel] = useState("Load from clipboard")
+    const [label, setLabel] = useState("Import")
 
-    function onClick() {
-        navigator.clipboard.readText()
-            .then(text => {
-                const squareData = JSON.parse(text)
-                if (isSquareData(squareData)) {
-                    setSquareData(squareData)
-                }
-            })
-            .catch(() => {
-                setLabel("Invalid data")
-                setTimeout(() => {
-                    setLabel("Load from clipboard")
-                }, 3000);
-            })
+    const [text, setText] = useState("")
+    const dialogRef = useRef<HTMLDialogElement>(null)
+    const [open, close] = useDialog(dialogRef.current)
+
+    function onClick2() {
+        open()
     }
-    return <button onClick={onClick}>{label}</button>
+
+    function onLoad() {
+        try {
+            const squareData = JSON.parse(text)
+            if (isSquareData(squareData)) {
+                setSquareData(squareData)
+                setLabel("Data loaded")
+                setTimeout(() => {
+                    setLabel("Import")
+                }, 3000);
+            } else {
+                throw new Error()
+            }
+        } catch (e) {
+            setLabel("Invalid data")
+            setTimeout(() => {
+                setLabel("Import")
+            }, 3000);
+        } finally {
+            setText("")
+            close();
+        }
+    }
+
+    return <>
+        <Dialog ref={dialogRef} >
+            <div className="load-dialog-container">
+                <div>Paste here:</div>
+                <textarea value={text} onChange={e => setText(e.target.value)} />
+                <button onClick={onLoad} disabled={!text}>Load</button>
+            </div>
+        </Dialog>
+        <button onClick={onClick2}>{label}</button>
+    </>
 }
 
 export default LoadFromClipboardButton
